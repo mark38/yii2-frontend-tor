58d61e11207587f77a6c2418ae0eb7d8bdeefb9c
Eugeniy Lobanov <eugeniy.lobanov@gmail.com>
2016-02-19 18:11:22 +0300

    Bottom-ads thumbnail

--- widgets/tor_ads/BottomAds.php
+++ widgets/tor_ads/BottomAds.php
@@ -21,25 +21,35 @@ class BottomAds extends InputWidget
 
     public function run()
     {
-        echo Html::beginTag('div', ['class' => 'row']);
+        echo Html::beginTag('div', ['class' => 'row bottom-ads']);
         /** @var $ad TorAds */
         foreach ($this->ads as $ad) {
             $image = $ad->gallery_groups_id && $ad->galleryGroup->galleryImage ? Html::img($ad->galleryGroup->galleryImage->small, ['class' => 'img-rounded', 'style' => 'width:100%;']) : Html::tag('small', 'Нет фото', ['class' => 'text-muted']);
 
-            echo '<div class="col-sm-4">';
+            echo '<div class="col-sm-6 col-md-3">';
             echo '<div class="row">' .
-                     '<div class="col-sm-4">' .
-                        '<div class="img-block">'.Html::a($image, [$ad->link->url, 'id' => $ad->id]).'</div>' .
-                        '<div>'.Html::tag('small', $ad->user->username.' ('.$ad->user->rating.')', ['class' => 'text-muted']).'</div>' .
-                     '</div><div class="col-sm-8">' .
-                        '<div>'.Html::a($ad->name, [$ad->link->url, 'id' => $ad->id]).'</div>' .
-                        '<div>'.Html::tag('small', mb_strimwidth($ad->description, 0, 70, "...", 'utf-8')).'</div>' .
-                        '<br>' .
-                        '<div class="row ad-price text-right">' .
-                            '<div class="col-sm-6 text-left">'.preg_replace('/\,00/', '', number_format($ad->price, 2, ',', '&thinsp;')).' руб.</div>' .
-                            '<div class="col-sm-6 text-left">'.preg_replace('/\,00/', '', number_format($ad->price, 2, ',', '&thinsp;')).' руб.</div>' .
-                        '</div>' .
-                     '</div>' .
+                        '<div class="thumbnail">'.
+                              Html::a($image, [$ad->link->url, 'id' => $ad->id]).
+                          '<div class="caption">
+                            <h3>'.Html::a($ad->name, [$ad->link->url, 'id' => $ad->id]).'</h3>
+                            <p>'.Html::tag('small', mb_strimwidth($ad->description, 0, 70, "...", 'utf-8')).'</p>
+                            <div class="row ad-price text-right">
+                                <div class="col-sm-6 text-left">'.preg_replace('/\,00/', '', number_format($ad->price, 2, ',', '&thinsp;')).' руб.</div> <div class="col-sm-6 text-left">'.preg_replace('/\,00/', '', number_format($ad->price, 2, ',', '&thinsp;')).' руб.</div>
+                            </div>
+                          </div>
+                        </div>' .
+//                     '<div class="col-sm-4">' .
+//                        '<div class="img-block">'.Html::a($image, [$ad->link->url, 'id' => $ad->id]).'</div>' .
+//                        '<div>'.Html::tag('small', $ad->user->username.' ('.$ad->user->rating.')', ['class' => 'text-muted']).'</div>' .
+//                     '</div><div class="col-sm-8">' .
+//                        '<div>'.Html::a($ad->name, [$ad->link->url, 'id' => $ad->id]).'</div>' .
+//                        '<div>'.Html::tag('small', mb_strimwidth($ad->description, 0, 70, "...", 'utf-8')).'</div>' .
+//                        '<br>' .
+//                        '<div class="row ad-price text-right">' .
+//                            '<div class="col-sm-6 text-left">'.preg_replace('/\,00/', '', number_format($ad->price, 2, ',', '&thinsp;')).' руб.</div>' .
+//                            '<div class="col-sm-6 text-left">'.preg_replace('/\,00/', '', number_format($ad->price, 2, ',', '&thinsp;')).' руб.</div>' .
+//                        '</div>' .
+//                     '</div>' .
                  '</div>';
             echo '</div>';
         }
